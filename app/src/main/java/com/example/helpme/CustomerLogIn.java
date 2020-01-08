808a0e44180126e666351d4374c36f6990b5a3ca
Hod <hod.gohasi@hotmail.com>
2020-01-08 17:50:34 +0200

    v1.0

--- app/src/main/java/com/example/helpme/CustomerLogIn.java
+++ app/src/main/java/com/example/helpme/CustomerLogIn.java
@@ -15,67 +15,50 @@ import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.auth.FirebaseAuth;
+
 import java.util.ArrayList;
 
 public class CustomerLogIn extends AppCompatActivity {
-    private final String CUSTOMER_NAME="customerName";
-    private final String NAME_OF_PLACE="nameOfPlace";
+
     private Button connectBtn;
-    private TextView nameEditText;
-    private ListView listView;
-    private boolean isChoosePlace=false;
-    private String nameOfChosePlace;
+
+
+
+    private EditText cellPhoneNumber;
+    private FirebaseAuth mFireBaseAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_customer_log_in);
         initViews();
-        choosePlace();
-        startCustomerLogInActivity();
+        connectAndStartListPlacesActivity();
     }
 
 
 
     private void initViews(){
         connectBtn=(Button)findViewById(R.id.customerConnectBTN);
-        nameEditText=(EditText)findViewById(R.id.customerEditName);
-        listView=(ListView)findViewById(R.id.listView);
-        createListViews();
-    }
 
-    private void createListViews() {
-        ArrayList<String> arrayList=new ArrayList<>();
-        arrayList.add("Renuar");
-        arrayList.add("Castro");
-        arrayList.add("Zara");
-        ArrayAdapter arrayAdapter=
-                new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
-        listView.setAdapter(arrayAdapter);
+        cellPhoneNumber=(EditText)findViewById(R.id.phone_num);
+        mFireBaseAuth=FirebaseAuth.getInstance();
 
     }
 
-    private void choosePlace() {
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
 
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                nameOfChosePlace = listView.getItemAtPosition(position).toString();
-                view.setBackgroundColor(Color.CYAN);
-                isChoosePlace=true;
-//                Log.d("test", "onItemClick: " + nameOfChosePlace);
-            }
-        });
-    }
 
-    private void startCustomerLogInActivity(){
+    private void connectAndStartListPlacesActivity(){
         connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if(isChoosePlace) {
-                    Intent intent = new Intent(CustomerLogIn.this, CustomerMain.class);
-                    intent.putExtra(CUSTOMER_NAME, nameEditText.getText().toString());
-                    intent.putExtra(NAME_OF_PLACE,nameOfChosePlace);
+                if(cellPhoneNumber.getText().toString().length()>=10) {
+                    Intent intent = new Intent(CustomerLogIn.this, ListPlacesActivity.class);
                     startActivity(intent);
-                }else{
+                }else if(cellPhoneNumber.getText().toString().length()<10){
+                    cellPhoneNumber.setError("Please enter your cellPhone number");
+                    cellPhoneNumber.requestFocus();
+                }
+                else{
                     Toast.makeText(CustomerLogIn.this,
                             "Choose the place where you are", Toast.LENGTH_SHORT).show();
                 }
---
+++ app/src/main/java/com/example/helpme/ListPlacesActivity.java
@@ -0,0 +1,83 @@
+package com.example.helpme;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.graphics.Color;
+import android.media.Image;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+public class ListPlacesActivity extends AppCompatActivity {
+    private final String CUSTOMER_NAME="customerName";
+    private final String NAME_OF_PLACE="nameOfPlace";
+    private ListView listView;
+    private String nameOfChosePlace;
+    private boolean isChoosePlace=false;
+    private TextView nameEditText;
+    private ImageView confirmBtn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list_places);
+        initViews();
+        createListViews();
+        choosePlace();
+        connectAndStartListPlacesActivity();
+    }
+
+    private void initViews(){
+        confirmBtn=(ImageView)findViewById(R.id.confirmBtn);
+        nameEditText=(EditText)findViewById(R.id.customerEditName);
+        listView=(ListView)findViewById(R.id.listView);
+        createListViews();
+    }
+    private void createListViews() {
+        ArrayList<String> arrayList=new ArrayList<>();
+        arrayList.add("Renuar");
+        arrayList.add("Castro");
+        arrayList.add("Zara");
+        ArrayAdapter arrayAdapter=
+                new ArrayAdapter(this,android.R.layout.simple_list_item_1,arrayList);
+        listView.setAdapter(arrayAdapter);
+
+    }
+
+    private void choosePlace() {
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                nameOfChosePlace = listView.getItemAtPosition(position).toString();
+                view.setBackgroundColor(Color.CYAN);
+                isChoosePlace=true;
+            }
+        });
+    }
+
+    private void connectAndStartListPlacesActivity(){
+        confirmBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(ListPlacesActivity.this, CustomerMain.class);
+                intent.putExtra(CUSTOMER_NAME, nameEditText.getText().toString());
+                startActivity(intent);
+            }
+        });
+
+
+    }
+
+}
